9433be172274337930d3788d099f68fc175c4bea
aaky.css <evanzzshrestha7@gmail.com>
2024-10-20 12:21:34 +0545

    Add files via upload

---
+++ face.py
@@ -0,0 +1,18 @@
+import cv2
+import sys
+imagePath = sys.argv[1]
+cascPath = "face.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30)
+)
+print("Detected {0} faces".format(len(faces)))
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Faces detected", image)
+cv2.waitKey(0)
